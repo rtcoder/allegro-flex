ae6762c8236cb4ee8fbffd1a26425b16097f5003
Dawid Jeż <dawidjez@gmail.com>
2024-09-21 20:42:45 +0200

    Enhance version handling on extension update
    Refactor to check previous extension version upon update and open options page if there's a version change. Saves current version to sync storage to track changes in future updates.

--- js/background.js
+++ js/background.js
@@ -1,5 +1,19 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.runtime.openOptionsPage();
+function handleUpdate(details) {
+    if (details.reason === 'update') {
+        const currentVersion = chrome.runtime.getManifest().version;
+        chrome.storage.sync.get('previousVersion', (result) => {
+            const previousVersion = result.previousVersion;
+
+            if (previousVersion && previousVersion !== currentVersion) {
+                chrome.runtime.openOptionsPage();
+            }
+
+            chrome.storage.sync.set({previousVersion: currentVersion});
+        });
+    }
+}
+chrome.runtime.onInstalled.addListener(details => {
+    handleUpdate(details);
 });
 chrome.action.onClicked.addListener(() => {
     chrome.runtime.openOptionsPage();
